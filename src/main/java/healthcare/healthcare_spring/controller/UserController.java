bb3422fdd168e3579aef7356aff5f4b4c8ed74be
MHWA2 <aosdb2021@gmail.com>
2024-06-07 19:02:09 +0900

    유저디비추가ì»컨트롤러

--- src/main/java/healthcare/healthcare_spring/controller/UserController.java
+++ src/main/java/healthcare/healthcare_spring/controller/UserController.java
@@ -1,7 +1,18 @@
 package healthcare.healthcare_spring.controller;
 
+import healthcare.healthcare_spring.dto.request.UserRequest;
+import healthcare.healthcare_spring.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    private final UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/user")
+    public void saveUser(@RequestBody UserRequest request) {userService.saveUser(request);}
 }
